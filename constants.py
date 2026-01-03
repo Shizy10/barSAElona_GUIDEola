5fa0a7e27fe2b6e697644b61720c63347e802a34
Shizy10 <ishirgaur@gmail.com>
2026-01-03 15:11:24 -0800

    By refactoring the clubs finances the owner realized how much money was being wasted by the managemnet. He fired the board and got the latent value for the current sequence and layer in his money making model as a result.

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 from pathlib import Path
-
+import torch
 SWISSPROT_FASTA_FILE = Path("/data/ishan/barSAElona_GUIDEola/uniprot_sprot.fasta")
+
+ESM_DIM = 1280
+SAE_DIM = 4096
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- interprot_sae.py
+++ interprot_sae.py
@@ -3,39 +3,42 @@ from transformers import AutoTokenizer, EsmModel
 from safetensors.torch import load_file
 from interprot.sae_model import SparseAutoencoder
 from huggingface_hub import hf_hub_download
+from constants import DEVICE, ESM_DIM, SAE_DIM
 
-ESM_DIM = 1280
-SAE_DIM = 4096
+def load_models(layer: int):
+    # Load ESM model
+    esm_model = EsmModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
+    esm_model.to(DEVICE)
+    esm_model.eval()
+
+    # Load SAE model
+    checkpoint_path = hf_hub_download(
+        repo_id="liambai/InterProt-ESM2-SAEs",
+        filename=f"esm2_plm{ESM_DIM}_l{layer}_sae{SAE_DIM}.safetensors"
+    )
+    sae_model = SparseAutoencoder(ESM_DIM, SAE_DIM)
+    sae_model.load_state_dict(load_file(checkpoint_path))
+    sae_model.to(DEVICE)
+    sae_model.eval()
+    return esm_model, sae_model
+
+
+def get_latents(seq: str, layer: int, esm_model: EsmModel, sae_model: SparseAutoencoder):
+    # Tokenize sequence and run ESM inference
+    tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D")
+    inputs = tokenizer(seq, padding=True, return_tensors="pt").to(DEVICE)
+    with torch.no_grad():
+        outputs = esm_model(**inputs, output_hidden_states=True)
+
+    # esm_layer_acts has shape (L+2, ESM_DIM), +2 for BoS and EoS tokens
+    esm_layer_acts_PA = outputs.hidden_states[layer][0]
+
+    # Using ESM embeddings from LAYER, run SAE inference
+    sae_acts_PZ = sae_model.get_acts(esm_layer_acts_PA) # (L+2, SAE_DIM)
+    return sae_acts_PZ
+
 LAYER = 24
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Load ESM model
-tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D")
-esm_model = EsmModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
-esm_model.to(device)
-esm_model.eval()
-
-# Load SAE model
-checkpoint_path = hf_hub_download(
-    repo_id="liambai/InterProt-ESM2-SAEs",
-    filename="esm2_plm1280_l24_sae4096.safetensors"
-)
-sae_model = SparseAutoencoder(ESM_DIM, SAE_DIM)
-sae_model.load_state_dict(load_file(checkpoint_path))
-sae_model.to(device)
-sae_model.eval()
-
 seq = "TTCCPSIVARSNFNVCRLPGTPEALCATYTGCIIIPGATCPGDYAN"
-
-# Tokenize sequence and run ESM inference
-inputs = tokenizer(seq, padding=True, return_tensors="pt").to(device)
-with torch.no_grad():
-    outputs = esm_model(**inputs, output_hidden_states=True)
-
-# esm_layer_acts has shape (L+2, ESM_DIM), +2 for BoS and EoS tokens
-esm_layer_acts = outputs.hidden_states[LAYER][0]
-
-# Using ESM embeddings from LAYER, run SAE inference
-sae_acts = sae_model.get_acts(esm_layer_acts) # (L+2, SAE_DIM)
-sae_acts
+esm_mod, sae_mod = load_models(LAYER)
+latents_PZ = get_latents(seq, LAYER, esm_mod, sae_mod)
+print(latents_PZ)
