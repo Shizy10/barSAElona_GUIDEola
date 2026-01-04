c2f2d970a9712ace22f0bcd7f5d2ee46fd8d9e18
Shizy10 <ishirgaur@gmail.com>
2026-01-03 21:16:58 -0800

    The trainer got really excited this week about a new gym methodology called convex optimization using an mean squard error and L1 loss. The team thought it would be a breeze but alas, they are spending more time in the gym than they thought they would.

--- constants.py
+++ constants.py
@@ -1,13 +1,21 @@
 from pathlib import Path
 import torch
 
+EMBEDDING_TYPES = ["sae", "emb"] 
+
+def linear_probe_weights(protein_name, layer_num, embedding_type):
+    protein_name = protein_name.lower()
+    embedding_type = embedding_type.lower()
+    if embedding_type not in EMBEDDING_TYPES:
+        raise NotImplementedError(f"embedding_type must be one of {EMBEDDING_TYPES}")
+    return DATA_FOLDER / f"{protein_name}_l{layer_num}_{embedding_type}_act.pt"
+
 DATA_FOLDER = Path("/data/ishan/barSAElona_GUIDEola")
 def linear_probe_dataset_path(protein_name, layer_num, embedding_type, filetype="hf"):
     protein_name = protein_name.lower()
     embedding_type = embedding_type.lower()
-    embedding_types = ["sae", "emb"] 
-    if embedding_type not in embedding_types:
-        raise NotImplementedError(f"embedding_type must be one of {embedding_types}")
+    if embedding_type not in EMBEDDING_TYPES:
+        raise NotImplementedError(f"embedding_type must be one of {EMBEDDING_TYPES}")
     return DATA_FOLDER / f"{protein_name}_l{layer_num}_{embedding_type}_act.{filetype}"
 
 SWISSPROT_FASTA_FILE = Path("/data/ishan/barSAElona_GUIDEola/uniprot_sprot.fasta")
